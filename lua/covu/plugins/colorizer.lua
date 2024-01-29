a96cc58e2a3fb495dc50899a34ff89eae20ba883
Jonathan <jonathan.schunk@icloud.com>
2024-01-29 01:33:37 +0100

    update

--- lua/covu/plugins/colorizer.lua
+++
@@ -1,9 +0,0 @@
-return {
-    "norcalli/nvim-colorizer.lua",
-    config = function()
-        require("colorizer").setup({
-            '*',
-        })
-        vim.api.nvim_set_keymap('n', '<Leader>ab', [[:ColorizerAttachToBuffer<CR>]], { noremap = true, silent = true })
-    end
-}
