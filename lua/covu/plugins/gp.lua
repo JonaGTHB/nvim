59dfc3541ed50b74247baef2884b85c487de77b1
Jonathan <jonathan.schunk@icloud.com>
2024-01-29 01:57:09 +0100

    update

--- lua/covu/plugins/gp.lua
+++ lua/covu/plugins/gp.lua
@@ -1,8 +1,10 @@
 return {
     "robitx/gp.nvim",
     config = function()
+        vim.keymap.set("n", "<leader>dgp", "<cmd>GpChatDelete<cr>", { noremap = true, silent = true })
         require("gp").setup({
             openai_api_key = "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",
+            chat_confirm_delete = false,
         })
     end,
 }
