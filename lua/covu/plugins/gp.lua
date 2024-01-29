039e79e2d0ef122b56c66b85b39eb5925413a46f
Jonathan <jonathan.schunk@icloud.com>
2024-01-29 01:50:05 +0100

    update

--- lua/covu/plugins/gp.lua
+++ lua/covu/plugins/gp.lua
@@ -1,6 +1,8 @@
 return {
     "robitx/gp.nvim",
     config = function()
-        require("gp").setup()
+        require("gp").setup({
+            openai_api_key = "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",
+        })
     end,
 }
