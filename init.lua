cc2c1aa5b429816da4372396be997145223e9cc7
Jonathan <jonathan.schunk@icloud.com>
2024-02-08 18:21:37 +0100

    update

--- init.lua
+++ init.lua
@@ -3,6 +3,14 @@ vim.g.mapleader = " "
 vim.o.termguicolors = true
 vim.opt.termguicolors = true
 
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = "tex",
+    callback = function()
+        vim.opt_local.textwidth = 80
+        vim.opt_local.formatoptions:append("t")
+    end,
+})
+
 require("covu.core")
 require("covu.lazy")
 require("covu.set")
--- lua/covu/plugins/lsp/mason-lspconfig.lua
+++ lua/covu/plugins/lsp/mason-lspconfig.lua
@@ -10,6 +10,13 @@ return {
             lua_ls = function()
                 local lua_opts = lsp_zero.nvim_lua_ls()
                 require('lspconfig').lua_ls.setup(lua_opts)
+                require 'lspconfig'.ltex.setup {
+                    settings = {
+                        ltex = {
+                            language = "de-DE",
+                        }
+                    }
+                }
             end,
         },
         automatic_installation = true,
